5435663f5bf206eed20cbb0599b4caabadde8007
Sean Maxwell <katanablaydz@yahoo.com>
2019-09-29 12:35:27 -0500

    Updated for docker

--- src/main/java/com/sean/SbaApplication.java
+++ src/main/java/com/sean/SbaApplication.java
@@ -1,18 +1,54 @@
 package com.sean;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.core.env.Environment;
 
 import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @EnableAdminServer
 @EnableDiscoveryClient
 @SpringBootApplication
 public class SbaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SbaApplication.class, args);
-	}
+        SpringApplication app = new SpringApplication(SbaApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        logApplicationStartup(env);
+    }
+
+    private static void logApplicationStartup(Environment env) {
+        String protocol = "http";
+        if (env.getProperty("server.ssl.key-store") != null) {
+            protocol = "https";
+        }
+        String serverPort = env.getProperty("local.server.port");
+        String contextPath = env.getProperty("server.servlet.context-path");
+        if ("".equals(contextPath) || contextPath == null) {
+            contextPath = "/";
+        }
+        String hostAddress = "localhost";
+        try {
+            hostAddress = InetAddress.getLocalHost().getHostAddress();
+        } catch (UnknownHostException e) {
+            log.warn("The host name could not be determined, using `localhost` as fallback");
+        }
+        log.info("\n----------------------------------------------------------\n\t" +
+                "Application '{}' is running! Access URLs:\n\t" +
+                "Local: \t\t{}://localhost:{}{}\n\t" +
+                "External: \t{}://{}:{}{}\n\t" +
+                "Profile(s): \t{}\n----------------------------------------------------------",
+            env.getProperty("spring.application.name"),
+            protocol, serverPort, contextPath,
+            protocol, hostAddress, serverPort, contextPath,
+            env.getActiveProfiles());
+    }
+
 
 }
